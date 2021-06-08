d43c1e599905365d468743a778c0203ac9c70b64
Yasen <362yasen2@gmail.com>
2021-06-07 18:43:07 -0700

    #
    initial

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const { Client } = require("@notionhq/client")
 require("dotenv").config();
 var faker = require('faker');
 
-
 // Initializing a client
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 })
-let database_ID = "7bc17afc973a4e4b84edbb5d835d41b6";
+let database_ID =process.env.STOCK_DB_ID;
 
 
 //Random User Generator
@@ -18,63 +17,79 @@ var UserGenerator = ()=>{
   return[Name,Email,Description];
 }
 
-
 //Post Data to the Database 
-(async () => {
-  const personData = UserGenerator();
-  const namedata = personData[0];
-  const emaildata = personData[1];
-  const descdata = personData[2];
-  const response = await notion.pages.create({
-    parent: {
-      database_id: "7bc17afc973a4e4b84edbb5d835d41b6",
-    },
-    properties: {
-      Name: {
-        title: [
-          {
-            text: {
-              content: namedata,
-            },
-          },
-        ],
+function storeListOfStocks(stockArray){
+  stockArray.forEach(element => {
+    (async () => {
+      const response = await notion.pages.create({
+        parent: {
+          database_id: process.env.STOCK_DB_ID,
+        },
+        properties: {
+          Ticker: {
+            title: [
+              {
+                text: {
+                  content: element,
+                },
+              },
+            ],
+          }
+            
+        }
+      });
+    })();
+  });
+
+}
+
+function storeOneStock(stock){
+  //adding stock to db
+  (async () => {
+    const response = await notion.pages.create({
+      parent: {
+        database_id: process.env.STOCK_DB_ID,
       },
-        Description: {
-          rich_text: [
+      properties: {
+        Ticker: {
+          title: [
             {
               text: {
-                content: descdata,
+                content: stock,
               },
             },
           ],
-        },
-        Email: {
-          email: emaildata
-        } 
-    }
-  });
-  
-})();
+        }    
+      }
+    });
+  })();
+}
 
 
-//Getting Data from Notion Database 
-(async () => {
-  const databaseId = database_ID;
-  const response = await notion.databases.query({
-    database_id: databaseId,
-  });
 
-  //map the response
-  const contact = response.results.map( (page)=>{
-    var name = page.properties.Name.title[0].text.content;
-    var email = page.properties.Email.email;
-    var desc = page.properties.Description.rich_text[0].text.content;
 
-    //log the data
-    console.log(name);
-    console.log(email);
-    console.log(desc);
+function getStockList(){
+  //Getting Data from Notion Database 
+  return (async () => {
+    var list=[];
+    const databaseId = database_ID;
+    const response = await notion.databases.query({
+      database_id: databaseId,
+    });
+
+    //map the response
+    let contact = response.results.map((page)=>{
+      var stock = page.properties.Ticker.title[0].text.content;
+      list.push(stock);
+    })
+    return list;
+  })();
+}
+
+function duplicateStockCheck(){
+  getStockList()
+  .then(list=>{
+    //do some duplication check
   })
-  
-})();
+}
 
